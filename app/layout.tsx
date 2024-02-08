cdf34363fc59fc691521e29e0b135a3d2ca23ca4
Philip Boen <philipboen001@gmail.com>
2024-02-08 19:33:49 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css";
+import React from "react";
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+
+const figtree = Figtree({
+  subsets: ["latin"],
+  variable: "--font-figtree",
+});
+
+export const metadata: Metadata = {
+  title: "BidConnect",
+  description: "The Ultimate Bidding Marketplace",
+  icons: {
+    icon: "/assets/icons/vercel.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${figtree.variable} max-container p-4 font-sans`}>
+        {children}
+      </body>
+    </html>
+  );
+}
